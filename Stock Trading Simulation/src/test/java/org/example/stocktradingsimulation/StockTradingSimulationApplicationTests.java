dc158c49eab4d80ab7e24d64bc13dd20cd68b190
DohaRamadan <77820526+DohaRamadan@users.noreply.github.com>
2024-07-25 04:05:33 +0300

    Added Initial Project

---
+++ Stock Trading Simulation/src/test/java/org/example/stocktradingsimulation/StockTradingSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stocktradingsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
